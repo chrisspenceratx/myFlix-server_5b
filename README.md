5bb444b3f7af97ce2082e0af3ec155af7475a2fb
chrisspenceratx <chrisspenceratx@gmail.com>
2023-08-10 07:09:53 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is the server side to an API, a resource to movie information.  To view/add to the project, use the following command:
+
+BASH:
+node index.js
+
+(Note: While developing this app, I ran into specific github branch conflicts/errors.  I tried to start fresh at different points, by making new repos/versions to compensate.  Now that it is fully functional, I created this final repo, 'myFlix-server_5b'.  This is why there are fewer commits for this particular repo.)
---
+++ passport.js
@@ -0,0 +1,62 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Models = require('./models');
+const passportJWT = require('passport-jwt');
+
+let Users = Models.User;
+let JSTStrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'Username',
+      passwordField: 'Password',
+    },
+    (username, password, callback) => {
+      Users.findOne({ Username: username })
+        .then((user) => {
+          if (!user) {
+            console.log('incorrect username');
+            return callback(null, false, {
+              message: 'Incorrect username or password.',
+            });
+          }
+
+          if (!user.validatePassword(password)) {
+            console.log('incorrect password');
+            return callback(null, false, {
+              message: 'Incorrect username or password.',
+            });
+          }
+
+          console.log('finished');
+          return callback(null, user);
+        })
+        .catch((error) => {
+          if (error) {
+            console.log(error);
+            return callback(error);
+          }
+        });
+    }
+  )
+);
+
+passport.use(
+  new JSTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'tryandguesstheansweritsreallyhard',
+    },
+    (jwtPayload, callback) => {
+      return Users.findById(jwtPayload._id)
+        .then((user) => {
+          return callback(null, user);
+        })
+        .catch((error) => {
+          return callback(error);
+        });
+    }
+  )
+);
