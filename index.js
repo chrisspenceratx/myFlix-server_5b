13a22b306d801943abfae33ee9196fe79ee8c4b4
chrisspenceratx <chrisspenceratx@gmail.com>
2023-09-01 17:11:35 -0500

    old commit

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const Users = Models.User;
 
 mongoose.set("strictQuery", false);
 // KEEP THESE HERE FOR EASY ACCESS WHEN MAKING LOCAL CHANGES //
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(morgan('common'));
 app.use(express.static('public'));
