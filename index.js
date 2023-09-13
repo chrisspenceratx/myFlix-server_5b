39fa0f433762b75bcea51e80d3546d05795ad19e
chrisspenceratx <chrisspenceratx@gmail.com>
2023-09-13 06:31:29 -0500

    reenabling cors

--- index.js
+++ index.js
@@ -21,19 +21,29 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
 
-// let allowedOrigins = ['*'];
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+const allowedOrigins = [
+  'http://localhost:8080',
+  'https://spencer-flix-c2b5a70a1e0d.herokuapp.com/',
+  'http://localhost:1234',
+  'mongodb://localhost:27017/myflixfinderdb',
+  'mongodb://localhost:27017'
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          "The CORS policy for this application doesn't allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
 
 let auth = require('./auth')(app);
 const passport = require('passport');
