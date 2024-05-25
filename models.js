851bc147aa5e2392a91381cc88a87dafe2e6f9d3
chrisspenceratx <chrisspenceratx@gmail.com>
2024-05-25 08:29:19 -0500

    use jsdoc for models.js / create `out` folder

--- models.js
+++ models.js
@@ -1,6 +1,28 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
+/**
+ * @typedef {Object} Genre
+ * @property {string} Name - The name of the genre.
+ * @property {string} Description - The description of the genre.
+ */
+
+/**
+ * @typedef {Object} Director
+ * @property {string} Name - The name of the director.
+ * @property {string} Bio - The biography of the director.
+ */
+
+/**
+ * @typedef {Object} Movie
+ * @property {string} Title - The title of the movie.
+ * @property {string} Description - The description of the movie.
+ * @property {Genre} Genre - The genre of the movie.
+ * @property {Director} Director - The director of the movie.
+ * @property {string[]} Actors - An array of actors in the movie.
+ * @property {string} ImageURL - The URL of the movie's image.
+ * @property {boolean} Featured - Whether the movie is featured.
+ */
 let movieSchema = mongoose.Schema({
   Title: { type: String, required: true },
   Description: { type: String, required: true },
@@ -17,6 +39,14 @@ let movieSchema = mongoose.Schema({
   Featured: Boolean,
 });
 
+/**
+ * @typedef {Object} User
+ * @property {string} Username - The username of the user.
+ * @property {string} Password - The hashed password of the user.
+ * @property {string} Email - The email of the user.
+ * @property {Date} [Birthday] - The birthday of the user.
+ * @property {mongoose.Schema.Types.ObjectId[]} FavoriteMovies - An array of favorite movies' IDs.
+ */
 let userSchema = mongoose.Schema({
   Username: { type: String, required: true },
   Password: { type: String, required: true },
@@ -25,10 +55,22 @@ let userSchema = mongoose.Schema({
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
 
+/**
+ * Hashes a password using bcrypt.
+ * @function
+ * @param {string} password - The password to hash.
+ * @returns {string} The hashed password.
+ */
 userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
 
+/**
+ * Validates a password with the hashed password.
+ * @function
+ * @param {string} password - The password to validate.
+ * @returns {boolean} True if the password is valid, false otherwise.
+ */
 userSchema.methods.validatePassword = function (password) {
   return bcrypt.compareSync(password, this.Password);
 };
